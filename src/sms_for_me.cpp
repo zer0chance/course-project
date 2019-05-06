5b47d8a46710aec723d57712fb24d61cda97bcaf
user.emaildeswier@bk.ru <deswier@bk.ru>
2019-05-06 21:05:12 +0700

    Add function to match SMS field

---
+++ src/sms_for_me.cpp
@@ -0,0 +1,16 @@
+#include "tarif.h"
+void sms_for_me(short difference_sms_plus, short difference_sms_minus, my_tarif my_list, data *list, int i) {
+    if (list[i].sms != -1) {
+        if ((((list[i].sms - my_list.sms) <= difference_sms_plus) && ((list[i].sms - my_list.sms) >= 0)) ||
+            (((list[i].sms - my_list.sms >= (-1) * difference_sms_minus) &&
+              (list[i].sms - my_list.sms) <= 0))) {
+            list[i].sms_b = true;
+ 
+        }
+    } else {
+        if (my_list.sms > 500)//bezlimit
+        {
+            list[i].sms_b = true;
+        }
+    }
+}
