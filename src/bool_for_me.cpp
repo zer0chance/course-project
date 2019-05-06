85be9985199c49562345a23b17e1458f65dd904b
user.emaildeswier@bk.ru <deswier@bk.ru>
2019-05-06 21:00:16 +0700

    Create function to match suitable tarrifs

---
+++ src/bool_for_me.cpp
@@ -0,0 +1,13 @@
+#include "tarif.h"
+void bool_for_me(short difference_gb_plus, short difference_gb_minus, short difference_min_plus, short difference_min_minus,
+                 short difference_sms_plus, short difference_sms_minus, my_tarif my_list, data *list) {
+ 
+    for (int i = 0; i < n; i++) {
+        {
+            gb_for_me(difference_gb_plus, difference_gb_minus, my_list, list, i);
+            min_for_me(difference_min_plus, difference_min_minus, my_list, list, i);
+            sms_for_me(difference_sms_plus, difference_sms_minus, my_list, list, i);
+            // mezh_for_me(my_list, list, i);
+        }
+    }
+}
