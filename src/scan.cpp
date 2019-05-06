6f59220a663a07fe822785253e35866288ed4d2a
user.emaildeswier@bk.ru <deswier@bk.ru>
2019-05-06 14:38:31 +0700

    Set codestyle

--- src/scan.cpp
+++ src/scan.cpp
@@ -1,73 +1,67 @@
- #include "tarif.h"
+#include "tarif.h"
 int main()
 {
     my_tarif my_list;
- 
- 
+
     printf("Amount of GB of internet you spend every mounth\n");
     scanf("%hi", &my_list.gb);
-    while(my_list.gb<0)
-    {
+    while (my_list.gb < 0) {
         printf("Invalid input \nAmount of GB of internet you spend every mounth\n");
         scanf("%hi", &my_list.gb);
     }
- 
- 
+
     printf("Amount of minutes you speak every mounth\n");
     scanf("%hi", &my_list.min);
-    while(my_list.min<0)
-    {
+    while (my_list.min < 0) {
         printf("Invalid input \nAmount of minutes you speak every mounth\n");
         scanf("%hi", &my_list.min);
     }
- 
- 
+
     printf("Amount of SMS you send every mounth\n");
     scanf("%hi", &my_list.sms);
-    while(my_list.sms<0)
-    {
+    while (my_list.sms < 0) {
         printf("Invalid input \nAmount of SMS you send every mounth\n");
         scanf("%hi", &my_list.sms);
     }
- 
+
     printf("Do you want your tariff to include calls abroad? (y/n)\n");
     scanf("%s", &my_list.min_mezhgorod);
-    while(my_list.min_mezhgorod!='y'&&my_list.min_mezhgorod!='n')
- 
+    while (my_list.min_mezhgorod != 'y' && my_list.min_mezhgorod != 'n')
+
     {
         printf("Do you want your tariff to include calls abroad? (y/n)\n");
         scanf("%s", &my_list.min_mezhgorod);
     }
- 
-    data *list;
+
+    data* list;
     list = new data[n];
     int i = 0;
     FILE* input;
- 
-    input = fopen("input.txt", "r");
-       while (fscanf(input, "%s %s %hi %hi %hi %hi %d", list[i].company, list[i].tarif, &list[i].gb, &list[i].min,
-               &list[i].sms, &list[i].min_mezhgorod, &list[i].price) != EOF) {
 
-        if(list[i].gb<-1||list[i].min<0||list[i].sms<-1||list[i].min_mezhgorod<0||list[i].price<0)
-        {
-            printf ("%d,Invalid input in %s %s\n",i,list[i].company, list[i].tarif);
+    input = fopen("input.txt", "r");
+    while (fscanf(input, "%s %s %hi %hi %hi %hi %d", list[i].company, list[i].tarif, &list[i].gb, &list[i].min,
+               &list[i].sms, &list[i].min_mezhgorod, &list[i].price)
+        != EOF) {
+
+        if (list[i].gb < -1 || list[i].min < 0 || list[i].sms < -1 || list[i].min_mezhgorod < 0 || list[i].price < 0) {
+            printf("%d,Invalid input in %s %s\n", i, list[i].company, list[i].tarif);
             return 0;
         }
         i++;
     }
- 
-    short difference_gb_plus=0;
-    short difference_gb_minus=0;
+
+    short difference_gb_plus = 0;
+    short difference_gb_minus = 0;
     difference_gb(&difference_gb_plus, &difference_gb_minus, my_list);
- 
-    short difference_min_plus=0;
-    short difference_min_minus=0;
+
+    short difference_min_plus = 0;
+    short difference_min_minus = 0;
     difference_min(&difference_min_plus, &difference_min_minus, my_list);
- 
-    short difference_sms_plus=0;
-    short difference_sms_minus=0;
+
+    short difference_sms_plus = 0;
+    short difference_sms_minus = 0;
     difference_sms(&difference_sms_plus, &difference_sms_minus, my_list);
- 
-    delete(list);
+
+    delete (list);
     return 0;
 }
