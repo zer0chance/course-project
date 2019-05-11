723dd8ffded10f9d3c4b9461d52e9c2faf5ece49
zer0chance <ier2000@yandex.ru>
2019-05-11 20:03:38 +0700

    Add index array in print function

--- src/print_tarif.cpp
+++ src/print_tarif.cpp
@@ -1,31 +1,31 @@
 #include "tarif.h"
 
-void print_my_tarif(data* list, int m, FILE* output)
+void print_my_tarif(data* list, int m, FILE* output, int* A)
 {
     for (int i = 0; i < m; i++) {
 
         if ((list[i].gb == -1) && (list[i].sms == -1)) {
-            fprintf(output, "%s %s gb:unlimited, min:%hi, sms:unlimited, mezhg=%hi, price:%d\n", list[i].company,
-                list[i].tarif,
-                list[i].min, list[i].min_mezhgorod, list[i].price);
+            fprintf(output, "%s %s gb:unlimited, min:%hi, sms:unlimited, mezhg=%hi, price:%d\n", list[A[i]].company,
+                list[A[i]].tarif,
+                list[A[i]].min, list[A[i]].min_mezhgorod, list[A[i]].price);
         } else {
-            if (list[i].gb == -1) {
-                fprintf(output, "%s %s gb:unlimited, min:%hi, sms:%hi, mezhg=%hi, price:%d\n", list[i].company,
-                    list[i].tarif,
-                    list[i].min, list[i].sms, list[i].min_mezhgorod, list[i].price);
+            if (list[A[i]].gb == -1) {
+                fprintf(output, "%s %s gb:unlimited, min:%hi, sms:%hi, mezhg=%hi, price:%d\n", list[A[i]].company,
+                    list[A[i]].tarif,
+                    list[A[i]].min, list[A[i]].sms, list[A[i]].min_mezhgorod, list[A[i]].price);
             }
-            if (list[i].sms == -1) {
-                fprintf(output, "%s %s gb:%hi, min:%hi, sms:unlimited, mezhg=%hi, price:%d\n", list[i].company,
-                    list[i].tarif,
-                    list[i].gb,
-                    list[i].min, list[i].min_mezhgorod, list[i].price);
+            if (list[A[i]].sms == -1) {
+                fprintf(output, "%s %s gb:%hi, min:%hi, sms:unlimited, mezhg=%hi, price:%d\n", list[A[i]].company,
+                    list[A[i]].tarif,
+                    list[A[i]].gb,
+                    list[A[i]].min, list[A[i]].min_mezhgorod, list[A[i]].price);
             }
         }
 
-        if ((list[i].gb != -1) && (list[i].sms != -1)) {
-            fprintf(output, "%s %s gb:%hi, min:%hi, sms:%hi, mezhg=%hi, price:%d\n", list[i].company, list[i].tarif,
-                list[i].gb,
-                list[i].min, list[i].sms, list[i].min_mezhgorod, list[i].price);
+        if ((list[A[i]].gb != -1) && (list[A[i]].sms != -1)) {
+            fprintf(output, "%s %s gb:%hi, min:%hi, sms:%hi, mezhg=%hi, price:%d\n", list[A[i]].company, list[A[i]].tarif,
+                list[A[i]].gb,
+                list[A[i]].min, list[A[i]].sms, list[A[i]].min_mezhgorod, list[A[i]].price);
         }
     }
 }
