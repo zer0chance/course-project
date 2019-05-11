51fefa46911ffd5452438512d0ca320eaaf72be6
zer0chance <ier2000@yandex.ru>
2019-05-11 20:20:13 +0700

    Change output format

--- src/print_tarif.cpp
+++ src/print_tarif.cpp
@@ -5,17 +5,17 @@ void print_my_tarif(data* list, int m, FILE* output, int* A)
     for (int i = 0; i < m; i++) {
 
         if ((list[i].gb == -1) && (list[i].sms == -1)) {
-            fprintf(output, "%s %s gb:unlimited, min:%hi, sms:unlimited, mezhg=%hi, price:%d\n", list[A[i]].company,
+            fprintf(output, "%-8s %-12s Gb: unlimited Min: %-4hi Sms: unlimited Abroad call: %-4hi Price: %-4d\n", list[A[i]].company,
                 list[A[i]].tarif,
                 list[A[i]].min, list[A[i]].min_mezhgorod, list[A[i]].price);
         } else {
             if (list[A[i]].gb == -1) {
-                fprintf(output, "%s %s gb:unlimited, min:%hi, sms:%hi, mezhg=%hi, price:%d\n", list[A[i]].company,
+                fprintf(output, "%-8s %-12s Gb: unlimited Min: %-4hi Sms: %-10hi Abroad call: %-4hi Price: %-4d\n", list[A[i]].company,
                     list[A[i]].tarif,
                     list[A[i]].min, list[A[i]].sms, list[A[i]].min_mezhgorod, list[A[i]].price);
             }
             if (list[A[i]].sms == -1) {
-                fprintf(output, "%s %s gb:%hi, min:%hi, sms:unlimited, mezhg=%hi, price:%d\n", list[A[i]].company,
+                fprintf(output, "%-8s %-12s Gb: %-10hi Min: %-4hi Sms: unlimited Abroad call: %-4hi Price: %-4d\n", list[A[i]].company,
                     list[A[i]].tarif,
                     list[A[i]].gb,
                     list[A[i]].min, list[A[i]].min_mezhgorod, list[A[i]].price);
@@ -23,7 +23,7 @@ void print_my_tarif(data* list, int m, FILE* output, int* A)
         }
 
         if ((list[A[i]].gb != -1) && (list[A[i]].sms != -1)) {
-            fprintf(output, "%s %s gb:%hi, min:%hi, sms:%hi, mezhg=%hi, price:%d\n", list[A[i]].company, list[A[i]].tarif,
+            fprintf(output, "%-8s %-12s Gb: %-4hi Min: %-10hi Sms: %-10hi Abroad call: %-4hi Price: %-4d\n", list[A[i]].company, list[A[i]].tarif,
                 list[A[i]].gb,
                 list[A[i]].min, list[A[i]].sms, list[A[i]].min_mezhgorod, list[A[i]].price);
         }
