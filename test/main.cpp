272766363697c907df07c4145bc44cc325977046
zer0chance <ier2000@yandex.ru>
2019-06-12 11:56:27 +0700

    Include tarif.h in test main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,18 @@
 #define CATCH_CONFIG_MAIN 
 #include "catch.hpp"
-#include "geometry.h"
+#include "tarif.h"
+
+TEST_CASE("Sorting by price")
+{
+
+
+    /*const float x1 = 1;
+    const float y1 = 1;
+    const float x2 = 1;
+    const float y2 = 11;
+    const float x3 = 11;
+    const float y3 = 1;
+    const float P = 20 + sqrt(200);
+
+    REQUIRE(Area(x1, y1, x2, y2, x3, y3, P) == 50);*/
+}
